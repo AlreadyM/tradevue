7d77dffb3b290b57d979bbce548376946ebe1e76
华 <hua@huadeMac-mini.local>
2018-12-18 17:19:11 +0800

    init project with express vue

---
+++ src/client/components/carry.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <a href="javascript:;" @click="loop1">开始变换</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 1,
+      methods: {
+        loop1:function(){
+            setInterval(function(){
+                this.msg +=1;
+            },1000)
+        }
+    }
+    }
+  }
+}
+</script>
